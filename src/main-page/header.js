2b734e9df0a38165127caccd79f5034afe18e55c
smzakaria <5980277+smzakaria@users.noreply.github.com>
2018-09-16 16:13:27 -0300

    Create a react app and deploy in git1

---
+++ src/main-page/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './globomantics.png';
+
+const Header = (props) => (
+<header className="row">
+    <div className="col-md-5">
+        <img src={logo} className="logo" alt="logo"></img>
+    </div>
+    <div className="col-md-7 mt-5 subtitle">
+        {props.subtitle}
+    </div>
+</header>
+
+);
+export default Header;
